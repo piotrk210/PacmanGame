9e65058d741315f12b728b942f180a51ad273c62
Piotr Jakubowski <piotrk210@wp.pl>
2022-09-11 21:31:20 +0200

    added main menu scene and simple canvas with simple script

---
+++ WoodlandGames Test/Assets/_PROJECT/Scripts/Data/Keys.cs
@@ -0,0 +1,11 @@
+namespace _PROJECT.Scripts.Data
+{
+    public class Keys 
+    {
+        public class SceneIndex
+        {
+            public const int MAIN_MENU = 0;
+            public const int PLAY_GROUND = 1;
+        }
+    }
+}
---
+++ WoodlandGames Test/Assets/_PROJECT/Scripts/MainMenu/MainMenuPanel.cs
@@ -0,0 +1,38 @@
+using _PROJECT.Scripts.Data;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _PROJECT.Scripts.MainMenu
+{
+    public class MainMenuPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private Button startGameButton;
+        [SerializeField]
+        private Button quiteGameButton;
+    
+        private void Start()
+        {
+            startGameButton.onClick.AddListener(StartGame);
+            quiteGameButton.onClick.AddListener(QuiteGame);
+        }
+
+        private void OnDestroy()
+        {
+            startGameButton.onClick.RemoveListener(StartGame);
+            quiteGameButton.onClick.RemoveListener(QuiteGame);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(Keys.SceneIndex.PLAY_GROUND);
+        }
+
+        private void QuiteGame()
+        {
+            Application.Quit();
+        }
+    
+    }
+}
